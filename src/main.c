34b961611100782db32d09770e538e6050daf6eb
Austin Gill <Notgnoshi@gmail.com>
2024-01-06 15:35:43 -0600

    Add basic CLI argument parsing

--- src/main.c
+++ src/main.c
@@ -1,7 +1,79 @@
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+typedef struct cli_args_t
 {
-    printf("Hello world\n");
+    bool verbose;
+    const char* puzzle;
+    // TODO: Add different input formats for the puzzle parsing?
+} cli_args_t;
+
+void usage(void)
+{
+    printf("Solve sudoku puzzles\n");
+    printf("Usage: ssolve [--help] [--verbose] <PUZZLE>\n");
+}
+
+cli_args_t parse_args(int argc, char* argv[])
+{
+    cli_args_t args = {0};
+
+    struct option options[] = {
+        {"help", no_argument, NULL, 'h'},
+        {"verbose", no_argument, NULL, 'v'},
+        {NULL, 0, NULL, 0},
+    };
+
+    while (true)
+    {
+        int option_index = 0;
+
+        int retval = getopt_long(argc, argv, "hv", options, &option_index);
+        if (retval == -1)
+        {
+            break;
+        }
+
+        switch (retval)
+        {
+        case 'h':
+            usage();
+            exit(EXIT_SUCCESS);
+            break;
+        case 'v':
+            args.verbose = true;
+            break;
+        case '?':
+            exit(EXIT_FAILURE);
+            break;
+        default:
+            fprintf(stderr, "Unhandled argument: '%s'\n", argv[optind - 1]);
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
+
+    while (optind < argc)
+    {
+        if (args.puzzle == NULL)
+        {
+            args.puzzle = argv[optind];
+        }
+        optind++;
+    }
+    if (args.puzzle == NULL)
+    {
+        fprintf(stderr, "Missing required positional argument <PUZZLE>\n");
+    }
+
+    return args;
+}
+
+int main(int argc, char* argv[])
+{
+    const cli_args_t args = parse_args(argc, argv);
+
     return 0;
 }
